8ecaaa9188f994531e8fbb99ac7559963a324d74
Freddy <90415615+veganbtw@users.noreply.github.com>
2021-09-12 18:32:15 +0200

    Feature/helloworld (#2)
    * implemented hello world
    
    * testingagainwithreadmefile
    
    * updated readme
    
    * Implemented and tested leap year function

--- Lecture01.Tests/UnitTest1.cs
+++ Lecture01.Tests/UnitTest1.cs
@@ -20,5 +20,44 @@ namespace Lecture01.Tests
             var output = writer.GetStringBuilder().ToString().Trim();
             Assert.Equal("Hello World!", output);
         }
+
+
+        //Three tests for the number "4", "100" and "400".
+        [Fact]
+        public void Check_If_Is_Leap_Year0()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            //Act
+            Program.IsLeapYear(4);
+            var output = writer.GetStringBuilder().ToString().Trim();
+            //Assert
+            Assert.Equal("True", output);
+        }
+        [Fact]
+        public void Check_If_Is_Leap_Year1()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            //Act
+            Program.IsLeapYear(100);
+            var output = writer.GetStringBuilder().ToString().Trim();
+            //Assert
+            Assert.Equal("False", output);
+        }
+        [Fact]
+        public void Check_If_Is_Leap_Year2()
+        {
+            //Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            //Act
+            Program.IsLeapYear(400);
+            var output = writer.GetStringBuilder().ToString().Trim();
+            //Assert
+            Assert.Equal("True", output);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # monkeyfart
+
+## First lecture
+
+Implement hello world using TDD and TBD
+In place of "dotnet run" use "dotnet run --project .\Lecture01\"
